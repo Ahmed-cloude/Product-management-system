01bac887aa877e338e970bb65f5acf7c6cd4675b
ahmed <salmoahmad079@gmail.com>
2025-10-05 15:41:33 +0300

    added side bar to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import Form from './form/page'
 import ProductList from './productList/page';
-
+import SideBar from '../app/sideBar/page'
 
 
 export default function Home() {
   return (
     
-      <div className='min-h-screen bg-slate-800 text-white font-mono '>
-        <h1 className='w-fit m-auto py-4'>PRODUCT MANAGEMENT STSTEM</h1>
-        <Form />
-        <ProductList />
+      <div className='flex min-h-screen bg-slate-800 text-white font-mono '>
+        <SideBar />
+       
+        <div className=' w-[100%]'>
+          <h1 className='w-fit m-auto py-4'>PRODUCT MANAGEMENT STSTEM</h1>
+          <Form />
+          <ProductList />
+        </div>
       </div>
 
   );
