6b2af32b3dc657b6dc6f188c251723f7920d21ec
ahmed <salmoahmad079@gmail.com>
2025-10-05 15:45:37 +0300

    added some dependencis

---
+++ src/app/sold/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from'../sideBar/page'
+const page = () => {
+    return (
+        <div className='flex bg-slate-800'>
+            <SideBar />
+            <div className='min-h-screen  w-[100%]'> sold</div>
+        </div>
+    )
+}
+
+export default page
