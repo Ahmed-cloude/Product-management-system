3a7648dda0cb827315560d23eb6a31df859dd613
ahmed <salmoahmad079@gmail.com>
2025-10-18 14:27:44 +0300

    Restyle show all inventory

--- src/app/inventory/assistant/page.tsx
+++ src/app/inventory/assistant/page.tsx
@@ -2,8 +2,6 @@
 import React,{useState,useEffect} from 'react'
 
 const page = () => {
-    
-    // const unPaidProducts = JSON.parse(localStorage.getItem('productsDB')||null);
     const [dataOfAll,setDataOfAll] = useState({
         Cash:0,
         Installment:0,
@@ -36,24 +34,24 @@ const page = () => {
             <div>
               
               <div >
-                <div className='text-white flex bg-blue-300 w-fit m-auto'>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>Cash*</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>Installment*</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>Income</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full '>Stayed</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>Unpaid*</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>Loss</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>All</div>
+                <div className='text-fuchsia-500 flex  w-fit m-auto'>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>Cash</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>Installment</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>Income</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full '>Stayed</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>Unpaid</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>Loss</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>All</div>
                   
                 </div>
-                <div className='text-white flex bg-blue-300 w-fit m-auto'>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Cash} $</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Installment} $</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Cash + dataOfAll.Installment} $</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full '>{dataOfAll.Stayed}</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Unpaid}</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Stayed + dataOfAll.Unpaid}</div>
-                  <div className='bg-red-300 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Cash + dataOfAll.Installment + dataOfAll.Stayed + dataOfAll.Unpaid}</div>
+                <div className='text-white flex  w-fit m-auto'>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Cash} $</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Installment} $</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Cash + dataOfAll.Installment} $</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full '>{dataOfAll.Stayed} $</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Unpaid} $</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Stayed + dataOfAll.Unpaid} $</div>
+                  <div className='bg-neutral-700 p-2 m-2 w-[100px] rounded-full'>{dataOfAll.Cash + dataOfAll.Installment + dataOfAll.Stayed + dataOfAll.Unpaid} $</div>
                   
                 </div> 
               </div>
